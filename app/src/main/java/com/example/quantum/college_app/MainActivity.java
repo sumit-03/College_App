55ff918a3ed1b956f35a63a7b6fffcd9975c52b2
sumit-03 <sk69@iitbbs.ac.in>
2018-07-29 15:04:49 +0530

    Initial commit

---
+++ app/src/main/java/com/example/quantum/college_app/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.quantum.college_app;
+
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView imageViewNext;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+            imageViewNext=(ImageView) findViewById(R.id.ivnext);
+            imageViewNext.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                  //  startActivity(new Intent(MainActivity.this,LoginActicity.class));
+                }
+            });
+
+    }
+}
