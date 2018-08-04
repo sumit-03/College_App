0072988884fea1cdccc115acaa43b436c8a48e04
GrayR00t <38848711+GrayR00t@users.noreply.github.com>
2018-08-04 10:08:01 +0530

    new activity

--- app/src/main/java/com/example/quantum/college_app/e_summit.java
+++ app/src/main/java/com/example/quantum/college_app/e_summit.java
@@ -1,5 +1,6 @@
 package com.example.quantum.college_app;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,12 @@ public class e_summit extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_e_summit);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        startActivity(new Intent(e_summit.this,MainActivity.class));
     }
 }
